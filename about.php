54cde13300fcf6acbdda4910dd7b941d60fb54fa
solomoreal <solomoreal@yahoo.com>
2017-11-15 13:13:23 +0100

    add the about page

---
+++ about.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
+
+    <title>About Us</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Custom styles for this template -->
+    <link href="carousel.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/about.css">
+    <link rel="stylesheet" type="text/css" href="css/about1.css">
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+    <div class="navbar-wrapper">
+      <div class="container">
+
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="index.html">HOUSE OF OJILE</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="index.html">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="ourservices.php">Our Services</a></li>
+                <li><a href="gallary.php">Gallary</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Categories <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Women</a></li>
+                    <li><a href="#">Men</a></li>
+                    <li><a href="#">Children</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li class="dropdown-header">Specials</li>
+                    <li><a href="#">Beads</a></li>
+                    <li><a href="#">T-shirt Branding</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+      </div> 
+ <div class="container">   
+<div class="row">
+        <div class="col-lg-7 col-lg-offset-1 pull-right " id="small_slider">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img class="first-slide" src="image/70.jpg" alt="First slide">
+         
+        </div>
+
+        
+        <div class="item">
+          <img class="second-slide" src="image/Fashion-Designer.jpg" alt="Second slide">
+          
+        </div>
+        <div class="item">
+          <img class="third-slide" src="image/slid4.jpg" alt="Third slide">
+          
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div><!-- /.carousel -->
+           
+        </div> 
+         <aside>
+              <div class=" col-lg-4">
+                
+                 
+                    <h3>About House Of Ojile</h3>
+                      <p>
+                          We at house of Ojile are all about meeting your fashion needs. We deal in high quality but affordable fabrics, our fashion designers are amonsgt the best in the country and professionallity is our watch word.
+                      </p>
+                      <h3>Our Vision</h3>
+                      <p>kvnkjkvkjckcbmnvmvcnvnmmv khfk hgf jfhgk  jfhkx gxxkxhx kdfhkxk xxkjxgkxjk fhgkxgx   jhgkxgkxhkxxg kfjghkxghxk</p>
+                      <h3>Our Misssion</h3> 
+                          <p>Come visit us and you will not be disappointe</p>
+                       <p>
+                          Check out our contact page for more information on how to <a href="contact.php">contact us</a>, learn more about all <a href="ourservices.php">our service. </a>
+                       </p>   
+
+                </div>
+              
+         </aside> 
+   </div>   
+</div>
+<hr class="featurette-divider"> 
+
+ <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2017 House Of Ojile fashion, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </footer>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="../../assets/js/vendor/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>         
+        
+   
+
+
+
+    
+  </body>
+ </html> 
+
+
+
